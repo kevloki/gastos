eda756457ba6f72939235957db70d1d2ef71b2bb
kevloki <keven.pgomes70@gmail.com>
2023-08-24 02:32:27 -0400

    okkk

--- gasto.py
+++ gasto.py
@@ -45,20 +45,22 @@ def main():
         cursor_bd = conexao_bd.cursor()
 
         valor_luz = obter_entrada_usuario("Digite o valor da conta de luz: ")
+        data_vencimento_luz = input("Digite a data de vencimento para a conta de luz (DD-MM-AAAA): ")
+        data_vencimento_luz_bd = formatar_data_para_bd(data_vencimento_luz)
+
         valor_wifi = obter_entrada_usuario("Digite o valor da conta de WiFi: ")
+        data_vencimento_wifi = input("Digite a data de vencimento para a conta de WiFi (DD-MM-AAAA): ")
+        data_vencimento_wifi_bd = formatar_data_para_bd(data_vencimento_wifi)
+
         valor_passe = obter_entrada_usuario("Digite o valor do passe de transporte: ")
+        data_vencimento_passe = input("Digite a data de vencimento para o passe de transporte (DD-MM-AAAA): ")
+        data_vencimento_passe_bd = formatar_data_para_bd(data_vencimento_passe)
+
         valor_recarga_celular = obter_entrada_usuario("Digite o valor da recarga do celular: ")
+        data_vencimento_celular = input("Digite a data de vencimento para a recarga do celular (DD-MM-AAAA): ")
+        data_vencimento_celular_bd = formatar_data_para_bd(data_vencimento_celular)
 
         gastos_extra = obter_gastos_extra()
-
-        while True:
-            data_vencimento = input("Digite a data de vencimento (DD-MM-AAAA): ")
-            data_vencimento_bd = formatar_data_para_bd(data_vencimento)
-            
-            if data_vencimento_bd is not None:
-                break  # Sair do loop se a data for válida
-            else:
-                print("Data inválida. Por favor, insira uma data válida.")
 
         total_a_pagar = (
             valor_luz + valor_wifi + valor_passe + valor_recarga_celular +
@@ -67,19 +69,28 @@ def main():
 
         valor_recebido = obter_entrada_usuario("Digite o valor que você irá receber este mês: ")
 
-        saldo_restante = valor_recebido - total_a_pagar
+        inserir_gastos(cursor_bd, [("Conta de Luz", valor_luz)], data_vencimento_luz_bd)
+        inserir_gastos(cursor_bd, [("Conta de WiFi", valor_wifi)], data_vencimento_wifi_bd)
+        inserir_gastos(cursor_bd, [("Passe de Transporte", valor_passe)], data_vencimento_passe_bd)
+        inserir_gastos(cursor_bd, [("Recarga do Celular", valor_recarga_celular)], data_vencimento_celular_bd)
 
-        inserir_gastos(cursor_bd, [("Conta de Luz", valor_luz), ("Conta de WiFi", valor_wifi),
-                                    ("Passe de Transporte", valor_passe), ("Recarga do Celular", valor_recarga_celular)] + gastos_extra,
-                                    data_vencimento_bd)
+        for nome_gasto, valor_gasto in gastos_extra:
+            data_vencimento = input(f"Digite a data de vencimento para o gasto '{nome_gasto}' (DD-MM-AAAA): ")
+            data_vencimento_bd = formatar_data_para_bd(data_vencimento)
+            if data_vencimento_bd is not None:
+                inserir_gastos(cursor_bd, [(nome_gasto, valor_gasto)], data_vencimento_bd)
+            else:
+                print(f"O gasto '{nome_gasto}' não foi inserido devido a uma data inválida.")
 
         conexao_bd.commit()
 
+        saldo_restante = valor_recebido - total_a_pagar
+
         print("\nResumo dos Gastos:")      
-        print(f"Conta de Luz: R${valor_luz:.2f}")
-        print(f"Conta de Wifi: R${valor_wifi:.2f}")
-        print(f"Passe de Transporte: R${valor_passe:.2f}")
-        print(f"Recarga do Celular: R${valor_recarga_celular:.2f}")
+        print(f"Conta de Luz: R${valor_luz:.2f} - Vencimento: {data_vencimento_luz}")
+        print(f"Conta de Wifi: R${valor_wifi:.2f} - Vencimento: {data_vencimento_wifi}")
+        print(f"Passe de Transporte: R${valor_passe:.2f} - Vencimento: {data_vencimento_passe}")
+        print(f"Recarga do Celular: R${valor_recarga_celular:.2f} - Vencimento: {data_vencimento_celular}")
         for nome_gasto, valor_gasto in gastos_extra:
             print(f"{nome_gasto}: R${valor_gasto:.2f}")
         print("------------------------")
@@ -96,3 +107,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
