53633b32d027b4da396f9703a827056521dabe13
keven <kevenhohure@gmail.com>
2023-09-11 16:01:30 -0400

    nova tela

--- gasto.py
+++ gasto.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from datetime import datetime
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
 CONFIG_BD = {
     "host": "localhost",
@@ -7,24 +9,6 @@ CONFIG_BD = {
     "password": "admin",
     "database": "gastos"
 }
-
-def obter_entrada_usuario(mensagem, tipo_dado=float):
-    while True:
-        try:
-            valor = tipo_dado(input(mensagem))
-            return valor
-        except ValueError:
-            print("Entrada inválida. Por favor, insira um valor válido.")
-
-def obter_gastos_extra():
-    gastos = []
-    while True:
-        nome = input("Digite o nome do gasto extra (ou pressione Enter para encerrar): ")
-        if not nome:
-            break
-        valor = obter_entrada_usuario(f"Digite o valor do gasto '{nome}': ")
-        gastos.append((nome, valor))
-    return gastos
 
 def formatar_data_para_bd(data):
     try:
@@ -39,29 +23,28 @@ def inserir_gastos(cursor, gastos, data):
     for nome, valor in gastos:
         cursor.execute(query_insercao, (nome, valor, data))
 
-def main():
+def calcular_gastos():
+    valor_luz = float(entrada_luz.get())
+    valor_passe = float(entrada_passe.get())
+    valor_recarga_celular = float(entrada_recarga.get())
+    data_vencimento_comum = entrada_data_vencimento.get()
+    valor_recebido = float(entrada_valor_recebido.get())
+
+    gastos_extra = []
+    for nome_gasto, valor_gasto in gastos_extra_entries:
+        valor_gasto = float(valor_gasto.get())
+        gastos_extra.append((nome_gasto, valor_gasto))
+
     try:
         conexao_bd = mysql.connector.connect(**CONFIG_BD)
         cursor_bd = conexao_bd.cursor()
 
-        # Coletar os valores dos gastos regulares
-        valor_luz = obter_entrada_usuario("Digite o valor da conta de luz: ")
-        valor_passe = obter_entrada_usuario("Digite o valor do passe de transporte: ")
-        valor_recarga_celular = obter_entrada_usuario("Digite o valor da recarga do celular: ")
-        
-        # Coletar a data de vencimento comum para gastos regulares
-        data_vencimento_comum = input("Digite a data de vencimento para os gastos regulares (DD-MM-AAAA): ")
         data_vencimento_comum_bd = formatar_data_para_bd(data_vencimento_comum)
-
-        # Coletar os valores dos gastos extras
-        gastos_extra = obter_gastos_extra()
 
         total_a_pagar = (
             valor_luz + valor_passe + valor_recarga_celular +
             sum(gasto[1] for gasto in gastos_extra)
         )
-
-        valor_recebido = obter_entrada_usuario("Digite o valor que você irá receber este mês: ")
 
         # Inserir gastos regulares com a mesma data de vencimento
         gastos_regulares = [("Conta de Luz", valor_luz), ("Passe de Transporte", valor_passe),
@@ -70,7 +53,7 @@ def main():
 
         # Inserir gastos extras com datas de vencimento individuais
         for nome_gasto, valor_gasto in gastos_extra:
-            data_vencimento = input(f"Digite a data de vencimento para o gasto '{nome_gasto}' (DD-MM-AAAA): ")
+            data_vencimento = gastos_extra_datas[nome_gasto].get()
             data_vencimento_bd = formatar_data_para_bd(data_vencimento)
             if data_vencimento_bd is not None:
                 inserir_gastos(cursor_bd, [(nome_gasto, valor_gasto)], data_vencimento_bd)
@@ -81,28 +64,75 @@ def main():
 
         saldo_restante = valor_recebido - total_a_pagar
 
-        print("\nResumo dos Gastos:")      
+        # Exibir resumo na interface gráfica
+        resumo_texto = f"\nResumo dos Gastos:\n"
         for nome_gasto, valor_gasto in gastos_regulares:
-            print(f"{nome_gasto}: R${valor_gasto:.2f} - Vencimento: {data_vencimento_comum}")
+            resumo_texto += f"{nome_gasto}: R${valor_gasto:.2f} - Vencimento: {data_vencimento_comum}\n"
         for nome_gasto, valor_gasto in gastos_extra:
-            print(f"{nome_gasto}: R${valor_gasto:.2f}")
-        print("------------------------")
-        print(f"Total a pagar: R${total_a_pagar:.2f}")
-        print(f"Saldo restante: R${saldo_restante:.2f}")
+            resumo_texto += f"{nome_gasto}: R${valor_gasto:.2f}\n"
+        resumo_texto += "------------------------\n"
+        resumo_texto += f"Total a pagar: R${total_a_pagar:.2f}\n"
+        resumo_texto += f"Saldo restante: R${saldo_restante:.2f}\n"
+
+        resultado_label.config(text=resumo_texto)
 
     except mysql.connector.Error as erro:
-        print("Erro:", erro)
+        messagebox.showerror("Erro", f"Ocorreu um erro: {erro}")
     finally:
         if cursor_bd:
             cursor_bd.close()
         if conexao_bd and conexao_bd.is_connected():
             conexao_bd.close()
 
-if __name__ == "__main__":
-    main()
+# Criação da interface gráfica
+tela = tk.Tk()
+tela.title("Calculadora de Gastos")
 
+# Elementos para entrada de dados
+tk.Label(tela, text="Valor da conta de luz:").pack()
+entrada_luz = tk.Entry(tela)
+entrada_luz.pack()
 
+tk.Label(tela, text="Valor do passe de transporte:").pack()
+entrada_passe = tk.Entry(tela)
+entrada_passe.pack()
 
+tk.Label(tela, text="Valor da recarga do celular:").pack()
+entrada_recarga = tk.Entry(tela)
+entrada_recarga.pack()
 
+tk.Label(tela, text="Data de vencimento comum (DD-MM-AAAA):").pack()
+entrada_data_vencimento = tk.Entry(tela)
+entrada_data_vencimento.pack()
 
+tk.Label(tela, text="Valor que você irá receber este mês:").pack()
+entrada_valor_recebido = tk.Entry(tela)
+entrada_valor_recebido.pack()
 
+# Adicionando gastos extras
+gastos_extra_entries = []
+gastos_extra_datas = {}
+gasto_extra_label = tk.Label(tela, text="Gastos Extras:")
+gasto_extra_label.pack()
+
+while True:
+    nome_gasto = tk.simpledialog.askstring("Gasto Extra", "Digite o nome do gasto extra (ou cancel para encerrar):")
+    if not nome_gasto:
+        break
+
+    gastos_extra_entries.append((nome_gasto, tk.Entry(tela)))
+    gastos_extra_entries[-1][1].pack()
+
+    tk.Label(tela, text=f"Data de vencimento para '{nome_gasto}' (DD-MM-AAAA):").pack()
+    gastos_extra_datas[nome_gasto] = tk.Entry(tela)
+    gastos_extra_datas[nome_gasto].pack()
+
+# Botão para calcular os gastos
+tk.Button(tela, text="Calcular Gastos", command=calcular_gastos).pack()
+
+# Rótulo para mostrar o resumo
+resultado_label = tk.Label(tela, text="", justify="left")
+resultado_label.pack()
+
+# Inicie a interface gráfica
+tela.mainloop()
